94275ecf7f62051d719ec776bc87a78def94012e
Akim Mamedov <akim99999999@gmail.com>
2023-10-14 00:42:58 +0700

    Remove bad files

--- worker.mjs
+++
@@ -1,6 +0,0 @@
-import WorkerContext from "./dist/worker/index.js"
-
-export const expose = WorkerContext.expose
-export const registerSerializer = WorkerContext.registerSerializer
-export const Transfer = WorkerContext.Transfer
-export const isWorkerRuntime = WorkerContext.isWorkerRuntime
